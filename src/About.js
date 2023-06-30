32908f08d889fe13c4802215e69922407cc36585
Ali Kaan Yayla <naklovcuas@gmail.com>
2023-06-30 13:39:16 +0300

    Updated screenshot

--- src/About.js
+++ src/About.js
@@ -3,9 +3,9 @@ export default About; function About() {
     return (
         <div className='about--wrapper'>
             <h3 className='about--title'>About</h3>
-            <p className='about--text'>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-            <h3 className='about--title'>Interests</h3>
-            <p className='about--text'>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p className='about--text'>I am a back-end developer who is interested in .Net Core and .Net technologies. To perform my job in the best possible way and enhance my skills, I am learning React Js.</p>
+            <h3 className='about--title'>Ask Me About</h3>
+            <p className='about--text'>.Net MVC, .Net Core, .Net Core API, Entity Framework, Identity, Html, Css, Bootstrap, MSSql, PostgreSql, AWS Cloud</p>
         </div>
       );
 }
